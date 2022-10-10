feaaa425f4870837f22f58773caa347cb2243bb9
vickky9ice <adeboyevictor18@gmail.com>
2022-10-10 21:39:16 +0100

    Task4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,26 +1,36 @@
 #include "sort.h"
 
 /**
- * shell_sort - Shell sort - Knuth Sequence
- * @array: array to sort
- * @size: size of array
- */
+  * shell_sort - A function tat sorts an array usin shell algorithm.
+  * @array: The array to sort.
+  * @size: The length of the array.
+  * Return: Nothing.
+  */
 void shell_sort(int *array, size_t size)
 {
-	int gap = 1, i, j, tmp;
+	unsigned int i = 0, j = 0, gap = 0;
+	int aux = 0;
 
-	while (gap < ((int)(size)))
-		gap = (3 * gap) + 1;
-	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
+	if (array == NULL || size < 2)
+		return;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	for (; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (i = gap; i < (int)size; i += 1)
+		for (i = gap; i < size; i++)
 		{
-			tmp = array[i];
+			aux = array[i];
+			for (j = i; j >= gap && array[j - gap] > aux;
+					j -= gap)
+			{
+				if (array[j] != array[j - gap])
+					array[j] = array[j - gap];
+			}
+			if (array[j] != aux)
+				array[j] = aux;
 
-			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
-				array[j] = array[j - gap];
-
-			array[j] = tmp;
 		}
 		print_array(array, size);
 	}
--- sort.h
+++ sort.h
@@ -1,7 +1,5 @@
-#ifndef SORT_H_
-#define SORT_H_
-
-#include <stdio.h>
+#ifndef __SORT__
+#define __SORT__
 #include <stdlib.h>
 
 /**
@@ -18,14 +16,34 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2);
-void selection_sort(int *array, size_t size);
-void swap_integers(int *a, int *b);
-void quick_sort(int *array, size_t size);
+/*======================================================================*/
+/*=================         Printing Algoritms        ==================*/
+/*======================================================================*/
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-listint_t *create_listint(const int *array, size_t size);
 
-#endif
+
+/*======================================================================*/
+/*=================          sorting Algoritms        ==================*/
+/*======================================================================*/
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+int partition(int *array, int lo, int hi, size_t size);
+void quick_s(int *array, int lo, int hi, size_t size);
+void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
+void merge_partition(size_t lo, size_t hi, int *array, int *base);
+void swap_root(int *array, size_t root, size_t hi, size_t size);
+
+#endif /* __SORT__ */
